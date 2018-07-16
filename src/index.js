d0996426d78905fae55483720e704617dd993db7
hugodu69 <hugogogo@protonmail.com>
2018-07-16 12:28:57 +0200

    erreurs verifiees

--- src/index.js
+++ src/index.js
@@ -12,3 +12,20 @@ app.listen(SERVER_PORT, () => {
 app.get("/", (req, res) => {
 	res.send("coucou")
 })
+
+//si on appel une mauvaise route le serveur renvois une erreur
+app.use((req, res, next) => {
+	const error = new Error('Not found')
+	error.message = 'route invalide'
+	error.status = 404
+	next(error)
+})
+//si le serveur 
+app.use((error, req, res, next) => {
+	res.status(error.status || 500)
+	return res.json({
+		error:{
+			msg: error.message
+		}
+	})
+})
